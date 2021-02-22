51b1ea449ea46e1fec3770f0acb23a819fb9dff9
Dimi Giannakos <12002222@student.pxl.be>
2021-02-22 13:29:59 +0100

    week 1

---
+++ VoorbeeldBestanden - Student/VoorbeeldBestanden/MainWindow.xaml.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace VoorbeeldBestanden
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStreamWiterReader_Click(object sender, RoutedEventArgs e)
+        {
+            // schrijven//
+            string pad = @"..\..\Bestanden\Testbestand.txt";
+            StreamWriter sw = new StreamWriter(pad);
+
+            //Naar tekstbestand schrijven.
+            sw.WriteLine("Volgorde van getallen: ");
+            for (int i = 1; i <= 10; i++)
+            {
+                sw.Write($"{i * 100} -");
+            }
+
+            sw.WriteLine();
+            sw.Write("Einde");
+
+            //sluiten van bestand.
+            sw.Close();
+
+            //=== Lezen ===
+            StreamReader sr = new StreamReader(pad);
+
+            // inlezen van bestand per record.
+            while (!sr.EndOfStream)
+            {
+                TxtResultaat.Text += $"{sr.ReadLine()}\n";
+            }
+
+            //Volledig bestand inlezen.
+            //TxtResultaat.Text = sr.ReadToEnd();
+
+            sr.Close();
+        }
+
+        private void BtnFileStream_Click(object sender, RoutedEventArgs e)
+        {
+            // pad instellen.
+            string pad = @"..\..\Bestanden\klas.txt";
+
+            TxtResultaat.Clear();
+
+            // === USING- statement === 
+            using (FileStream fs = new FileStream(pad, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    //bestand lezen naar Textbox.
+                    TxtResultaat.Text = sr.ReadToEnd();
+                }
+            }
+
+
+        }
+
+        private void BtnFileStreamWriter_Click(object sender, RoutedEventArgs e)
+        {
+            //Pad instellen.
+            string schijfpad = @"..\..\Bestanden\klaswrite.txt";
+            string leespad = @"..\..\Bestanden\klas.txt";
+
+            //Tekstbestand schrijven vanuit tekstbestand. 
+            using (FileStream fs = new FileStream(schijfpad, FileMode.Create, FileAccess.Write))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    using (StreamReader sr = new StreamReader(leespad))
+                    {
+                        string naam = string.Empty;
+
+                        //schrijven en lezen per regel.
+                        while (!sr.EndOfStream)
+                        {
+                            naam = sr.ReadLine();
+                            sw.WriteLine(naam);
+                            LbxNamen.Items.Add(naam);
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private void BtnKlasseFile_Click(object sender, RoutedEventArgs e)
+        {
+            string pad = @"..\..\Bestanden\vakken.txt";
+            TxtResultaat.Clear();
+
+            if (!File.Exists(pad))
+            {
+                using (StreamWriter sw = File.CreateText(pad))
+                {
+                    sw.WriteLine("Programmeren - Lector: p.Briers | K.Palmaers");
+                    sw.WriteLine("web - Lector: p.Briers | p.Janssen");
+                    sw.WriteLine("it- organisation - Lector: p.Briers | K.Palmaers");
+                }
+            }
+
+            // (1) === Dadelijk inlezen ===.
+            TxtResultaat.Text = File.ReadAllText(pad);
+
+            //// (2)Opent het bestand om te lezen. 
+            //using(StreamReader sr = File.OpenText(pad))
+            //{
+            //    string s = string.Empty;
+            //    while(!sr.EndOfStream)
+            //    {
+            //        TxtResultaat.AppendText($"{sr.ReadLine()}\n");
+            //    }
+            //}
+
+            //// (3)=== OF dadelijk in array inlezen === 
+            //string[] gegevens = File.ReadAllLines(pad);
+            //TxtResultaat.Text = string.Join("\n", gegevens);
+
+            //foreach(string item in gegevens)
+            //{
+            //    LbxNamen.Items.Add(item);
+            //}
+
+        }
+
+        private void BtnWriteAllText_Click(object sender, RoutedEventArgs e)
+        {
+            string pad = @"..\..\Bestanden\Namenfile.txt";
+
+            if (!File.Exists(pad))
+            {
+                string lijn = $"OPLEIDINGSONDERDEEL: C# ADVANCED \n";
+                File.WriteAllText(pad, lijn);
+
+                // vermits je met file maar 1x een writealltext kan gebruiken moet je de daaropvol
+
+                File.AppendAllText(pad, "Klas: 1PRO A+B - dagopleiding  \n");
+                File.AppendAllText(pad, "Klas: 1PRO C+D - dagopleiding  \n");
+                File.AppendAllText(pad, "Klas: 1PRO E+F - dagopleiding  \n");
+                File.AppendAllText(pad, "Klas: 1PRO G+H - dagopleiding  \n");
+                File.AppendAllText(pad, "\n");
+                File.AppendAllText(pad, "Klas: 1PRW A+B - avondopleiding  \n");
+            }
+
+            //Bestand openen.
+            TxtResultaat.Clear();
+            TxtResultaat.Text = File.ReadAllText(pad);
+        }
+
+        private void BtnWriteCsv_Click(object sender, RoutedEventArgs e)
+        {
+            string[] velden;
+            TxtResultaat.Clear();
+
+            using (StreamReader sr = new StreamReader(@"..\..\Bestanden\klasCsv.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    velden = sr.ReadLine().Split(';');
+                    TxtResultaat.Text += $"Naam: {velden[0],-15} Voornaam:{velden[1]}\n";
+                }
+            }
+        }
+
+        private void BtnFixRead_Click(object sender, RoutedEventArgs e)
+        {
+            TxtResultaat.Clear();
+
+
+            using (StreamReader sr = new StreamReader(@"..\..\Bestanden\klasVast.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string lijn = sr.ReadLine();
+                    string veld1 = lijn.Substring(0, 19).Trim();
+                    string veld2 = lijn.Substring(20, 9).Trim();
+                    string veld3 = lijn.Substring(30, 14).Trim();
+                    TxtResultaat.Text += $"{veld2} {veld1} werkt in {veld3}" + Environment.NewLine;
+                }
+
+
+
+            }
+        }
+    }
+}
---
+++ VoorbeeldBestanden - Student/email/MainWindow.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+
+namespace email
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Dictionary<string, string> dicGeg = new Dictionary<string, string>();
+        private StringBuilder InlezenBestand(string file)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] velden;
+
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    int i = 1;
+                    while (!sr.EndOfStream)
+                    {
+                        // Scheidingsteken opgeven
+                        velden = sr.ReadLine().Split(',');
+                        if (velden.Length != 2) // lege regel, geen ',' tussen velden of een veld ontbreekt
+                        {
+                            MessageBox.Show($"Onvolledige gegevens bij lijn {i}");
+                        }
+                        else
+                        {
+                            sb.Append($"{velden[0].Replace("\"", ""),-20} :" +
+                                      $"{velden[1].Replace("\"", "")}").AppendLine();
+                        }
+                        i++;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Probleem met het inlezen van het bestand.", "Foutmelding", MessageBoxButton.OK, MessageBoxImage.Warning);
+                throw;
+            }
+
+            return sb;
+
+        }
+
+        private void BtnInlezen_Click(object sender, RoutedEventArgs e)
+        {
+            //Functie inlezenbestand dat een stringbuilder teruggeeft.
+            StringBuilder sb = InlezenBestand("email.txt");
+
+            if(sb.Length != 0)
+            {
+                TxtResultaat.Text = sb.ToString();
+            }
+        }
+
+        private void BtnInlezenDialog_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Filter = "Alle bestanden (*.*)|*.*|Tekstbestanden (*.text) | *.txt",
+                FilterIndex = 2,
+                FileName = "email.txt",
+                InitialDirectory = Environment.CurrentDirectory // onder ..\debug
+            };
+
+            if (ofd.ShowDialog() == true)
+            {
+                StringBuilder sb = InlezenBestand(ofd.FileName);
+
+                if(sb.Length != 0)
+                {
+                    TxtResultaat.Text = sb.ToString();
+                }
+            }
+
+        }
+
+        private void BtnInlezenDictionary_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] velden;
+
+
+            try
+            {
+                using (StreamReader sr = new StreamReader("email.txt"))
+                {
+                    int i = 1;
+                    while (!sr.EndOfStream)
+                    {
+                        // Scheidingsteken opgeven
+                        velden = sr.ReadLine().Split(',');
+                        if (velden.Length != 2) // lege regel, geen ',' tussen velden of een veld ontbreekt
+                        {
+                            MessageBox.Show($"Onvolledige gegevens bij lijn {i}");
+                        }
+                        else
+                        {
+                            dicGeg.Add(velden[0].Replace("\"", ""), velden[1].Replace("\"", ""));
+                        }
+                        i++;
+                    }
+                }
+                foreach (var item in dicGeg)
+                {
+                    TxtResultaat.Text += $"{item.Key,-20}: {item.Value}\n";
+                }
+                BtnWegschrijven.IsEnabled = true;
+
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show("Geef nieuwe naam!\n\n" + ex.Message, "Foutmelding"
+                , MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Probleem met het inlezen van het bestand.", "Foutmelding", MessageBoxButton.OK, MessageBoxImage.Warning);
+                throw;
+            }
+            
+        }
+
+        private void BtnWegschrijven_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using(StreamWriter sw= File.CreateText("Adressen.txt"))
+                {
+                    foreach(var item in dicGeg)
+                    {
+                        sw.WriteLine(item.Value);
+                    }
+                }
+                MessageBox.Show(@"E-mailadressen weggeschreven in ..\Adressen.txt.", "Info",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Kan bestand niet wegschrijven.", "Foutmelding", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+        }
+
+        private void BtnSluiten_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
